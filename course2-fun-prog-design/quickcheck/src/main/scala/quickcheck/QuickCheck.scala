fe5294bf4e8f9640ba02f537e3a1646dbe67c62f
jmontroy90 <jmontroy90@gmail.com>
2019-02-17 17:24:49 -0500

    finished quickcheck, maybe might clean a little...

--- course2-fun-prog-design/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ course2-fun-prog-design/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -1,11 +1,12 @@
 package quickcheck
 
 import common._
-
 import org.scalacheck._
 import Arbitrary._
 import Gen._
 import Prop._
+
+import scala.util.Try
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
@@ -22,11 +23,26 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     else Some(findMin(h))
   }
 
+  // println(s"\tnewMin = $newMin")
+  def isOrdered(_h: H, priorMin: Option[Int] = None): Boolean = {
+    if (isEmpty(_h)) true
+    else {
+      val newMin = findMin(_h)
+      val newHeap = deleteMin(_h)
+      if (priorMin.isDefined) {
+        if (newMin < priorMin.get) false
+        else isOrdered(newHeap, Some(newMin))
+      } else {
+        isOrdered(newHeap, Some(newMin))
+      }
+    }
+  }
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   // if you find the minimum of a heap, and then insert that same minimum back into the same heap, you should
   // still get the same minimum.
-  property("gen1") = forAll { (h: H) =>
+  property("gen1") = forAll { h: H =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
   }
@@ -46,26 +62,28 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     else finalMin == i2
   }
 
+  property("contains all inserts") = forAll { (i1: Int, i2: Int) =>
+    val h = insert(i1, insert(i2, empty))
+
+    val i1n = findMin(h)
+    val i2n = findMin(deleteMin(h))
+
+    Set(i1n, i2n) == Set(i1, i2)
+  }
+
   property("insert-empty-delete-min") = forAll { i: Int =>
     val h1 = insert(i, empty)
     val finalHeap = deleteMin(h1)
     isEmpty(finalHeap)
   }
 
+  property("insert-arbitrary-delete-min") = forAll { (h: H, i: Int) =>
+    val h1 = insert(i, h)
+    val finalHeap = deleteMin(h1)
+    isEmpty(finalHeap) == isEmpty(h)
+  }
+
   property("repeated-min-yields-ordered-list") = forAll { h: H =>
-    def isOrdered(_h: H, priorMin: Option[Int] = None): Boolean = {
-      if (isEmpty(_h)) true
-      else {
-        val newMin = findMin(_h)
-        val newHeap = deleteMin(_h)
-        if (priorMin.isDefined) {
-          if (newMin < priorMin.get) false
-          else isOrdered(newHeap, Some(newMin))
-        } else {
-          isOrdered(newHeap, Some(newMin))
-        }
-      }
-    }
     isOrdered(h)
   }
 
@@ -86,4 +104,18 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
       else min1.get == meldedMin.get
     }
   }
+
+  property("a melded heap should yield the same order as the two heaps in order") = forAll { (h1: H, h2: H) =>
+    def toList(h: H): List[A] = {
+      def toListInner(h: H, interm: List[A]): List[A] = {
+        if (isEmpty(h)) interm
+        else toListInner(deleteMin(h), findMin(h) :: interm)
+      }
+      toListInner(h, List.empty[A])
+    }
+
+    toList(meld(h1, h2)).reverse == (toList(h1) ++ toList(h2)).sorted
+  }
+
+
 }
